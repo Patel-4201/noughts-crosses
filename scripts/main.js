5e0ad270f3f854de8b1799f14c79e4542a52d081
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-09-23 14:30:40 +0000

    add some functions

--- scripts/main.js
+++ scripts/main.js
@@ -14,18 +14,96 @@
 
 // double();
 
+// const resetBtn = document.querySelector(".btn-reset");
+// const gameGrid = document.querySelector(".game-grid");
+
+// const color = ["pink", "blue", "yellow", "orange", "red"];
+// let count = 0;
+
+// resetBtn.addEventListener("click", function () {
+// alert("hii");
+//   resetBtn.style.background = "red";
+
+//   resetBtn.classList.remove("active");
+// if (count > 5) count = count - 6;
+// gameGrid.style.background = color[count];
+// count = count + 1;
+// });
+
+// const fruits = ["orange", "cheeku", "apple", "guvava"];
+// const b = fruits.shift();
+// // const a = fruits.pop();
+
+// console.log(b)
+// const num = ["orange", "cheeku", "apple", "guvava"];
+// function getBetween(input) {
+//   return input.slice(1, input.length - 1);
+// }
+// const x = getBetween(num);
+// console.log(x);
+
+// console.log(num);
+
+// const fruits = ["orange", "cheeku", "apple", "guvava"];
+// const a = fruits.join("_");
+
+// console.log(a);
+
+// function table(num) {
+//   for (let i = 1; i <= 10; i++);
+//   // console.log(`${num}x${i}=${num * i}`);
+//   console.log(num + "x" + i + "=" + num * i);
+// }
+// table(3);
+
+// const nums = [45, 78, 525, 12, 564];
+
+// for (let k = 0; k < nums.length; k++) {
+//   nums[k] = nums[k] * 3;
+// }
+
+// console.log(nums);
+
+// callback function,no name
+// nums.forEach(function (v, idx) {
+//   // console.log(a * 2);
+//   nums[idx] = v * 2;
+// });
+// console.log(nums);
+
+// const x = nums.map(function (value) {
+//   return value % 2 == 0;
+// });
+// console.log(x);
+
+const headingEl = document.querySelector(".heading");
+const currentPlayerEl = document.querySelector(".current-player");
+const boxes = document.querySelectorAll(".box");
 const resetBtn = document.querySelector(".btn-reset");
-const gameGrid = document.querySelector(".game-grid");
 
-const color = ["pink", "blue", "yellow", "orange", "red"];
-let count = 0;
+// ===== Game Variables =====
+const players = ["O", "X"];
+let currPlayer = getPlayers();
 
-resetBtn.addEventListener("click", function () {
-  // alert("hii");
-  //   resetBtn.style.background = "red";
+currentPlayerEl.textContent = currPlayer;
+// functions
+function getPlayers() {
+  const rn = Math.floor(Math.random() * 2);
+  return players[rn];
+}
 
-  //   resetBtn.classList.remove("active");
-  if (count > 5) count = count - 6;
-  gameGrid.style.background = color[count];
-  count = count + 1;
+function swapPlayer() {
+  currPlayer = currPlayer === "O" ? "X" : "O";
+}
+
+function handleClick(input) {
+  input.textContent = currPlayer;
+  swapPlayer();
+  currentPlayerEl.textContent = currPlayer;
+}
+// only the first one
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    handleClick(box);
+  });
 });
