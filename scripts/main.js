3674dd1c677e2ff43b96bd19b2071a3d38344c47
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-09-30 08:04:07 +0000

    add reset function

--- scripts/main.js
+++ scripts/main.js
@@ -1,82 +1,5 @@
-// const userInput = prompt("enter something");
-
-// console.log(userInput);
-
-// const username = "weekend";
-
-// console.log(`hello ${username}`);
-
-// function double() {
-//   const userInput = prompt("enter number");
-//   const userdouble = userInput * 2;
-//   console.log(userdouble);
-// }
-
-// double();
-
-// const resetBtn = document.querySelector(".btn-reset");
-// const gameGrid = document.querySelector(".game-grid");
-
-// const color = ["pink", "blue", "yellow", "orange", "red"];
-// let count = 0;
-
-// resetBtn.addEventListener("click", function () {
-// alert("hii");
-//   resetBtn.style.background = "red";
-
-//   resetBtn.classList.remove("active");
-// if (count > 5) count = count - 6;
-// gameGrid.style.background = color[count];
-// count = count + 1;
-// });
-
-// const fruits = ["orange", "cheeku", "apple", "guvava"];
-// const b = fruits.shift();
-// // const a = fruits.pop();
-
-// console.log(b)
-// const num = ["orange", "cheeku", "apple", "guvava"];
-// function getBetween(input) {
-//   return input.slice(1, input.length - 1);
-// }
-// const x = getBetween(num);
-// console.log(x);
-
-// console.log(num);
-
-// const fruits = ["orange", "cheeku", "apple", "guvava"];
-// const a = fruits.join("_");
-
-// console.log(a);
-
-// function table(num) {
-//   for (let i = 1; i <= 10; i++);
-//   // console.log(`${num}x${i}=${num * i}`);
-//   console.log(num + "x" + i + "=" + num * i);
-// }
-// table(3);
-
-// const nums = [45, 78, 525, 12, 564];
-
-// for (let k = 0; k < nums.length; k++) {
-//   nums[k] = nums[k] * 3;
-// }
-
-// console.log(nums);
-
-// callback function,no name
-// nums.forEach(function (v, idx) {
-//   // console.log(a * 2);
-//   nums[idx] = v * 2;
-// });
-// console.log(nums);
-
-// const x = nums.map(function (value) {
-//   return value % 2 == 0;
-// });
-// console.log(x);
+// ===== import another js file =====
 import chances from "./chances";
-//noughts-crosses
 
 const headingEl = document.querySelector(".heading");
 const currentPlayerEl = document.querySelector(".current-player");
@@ -97,13 +20,13 @@ function getPlayer() {
 function swapPlayer() {
   const newPlayer = currPlayer === "O" ? "X" : "O";
   currPlayer = newPlayer;
-  currentPlayerEl.textContent = newPlayer;
+  currentPlayerEl.textContent = currPlayer;
 }
 
 // =====Start the game =====
 function startGame() {
   // current player set the random player
-  console.log("1");
+  // console.log("1");
   currPlayer = getPlayer();
   virtualGrid = new Array(9).fill("");
   currentPlayerEl.textContent = currPlayer;
@@ -126,7 +49,7 @@ function handleBoxClick(input) {
   }
 }
 
-console.log(boxes);
+// console.log(boxes);
 
 boxes.forEach(function (_, index) {
   _.addEventListener("click", function () {
@@ -136,7 +59,15 @@ boxes.forEach(function (_, index) {
 
 function handleReset() {
   //TODO:
-  alert("hii");
+  // alert("hii");
+  // Clear the virtual grid
+  virtualGrid = new Array(9).fill("");
+
+  // Clear the text content of each box
+  boxes.forEach((box) => {
+    box.textContent = "";
+    box.style.cursor = "pointer"; // Reset cursor style
+  });
 }
 
 resetBtn.addEventListener("click", handleReset);
